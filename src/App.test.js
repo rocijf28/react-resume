0b22023cb0b9286de2cb163cef45e7e1c6f88665
Rocío Justo Fernández <rocio.justo@informatica.cifpaviles.pa>
2025-01-23 19:29:06 +0100

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import data from "./data.json";
 
-test('renders learn react link', () => {
+test('aplicar tema por defecto', () => {
+  // Mock `window.matchMedia`
+  window.matchMedia = jest.fn().mockImplementation((query) => {
+    return {
+      matches: query === '(prefers-color-scheme: dark)',
+      media: query,
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    };
+  });
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const htmlElement = document.documentElement;
+  const appliedTheme = htmlElement.getAttribute('data-theme');
+
+  expect(['light', 'dark']).toContain(appliedTheme);
+});
+
+test('renderiza toda la informacion', () => {
+  render(<App />);
+
+  // Verificar que los componentes clave están presentes
+  expect(screen.getByText(data.info.name)).toBeInTheDocument(); // Header
+  expect(screen.getByText(data.experience[0].company)).toBeInTheDocument(); // ExperienceSection
+  expect(screen.getByText(data.skills[0].name)).toBeInTheDocument(); // SkillsSection
+  expect(screen.getByText(data.education[0].degree)).toBeInTheDocument(); // EducationSection
 });
